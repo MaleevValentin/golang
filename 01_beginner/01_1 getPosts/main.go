3e4629ca9e0be7440e4898ea1d5b986211026f8b
Maleev Valentin <maleyev.valentin@gmail.com>
2021-06-11 21:34:24 +0300

    add: exercise 1 - get posts

---
+++ 01_beginner/01_1 getPosts/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	getInfo := string(body)
+	fmt.Println(getInfo)
+}
